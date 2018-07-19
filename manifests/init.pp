43876f60fa540329c81a0fc68f5586419438fbbe
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-07-19 12:06:18 -0400

    url

--- manifests/init.pp
+++ manifests/init.pp
@@ -63,7 +63,7 @@ class nubis_alertmanager($version = '0.8.0', $tag_name='monitoring', $project=un
   }
 
   systemd::unit_file { 'alertmanager.service':
-    source => "puppet:///${module_name}/files/alertmanager.systemd",
+    source => "puppet:///${module_name}/alertmanager.systemd",
   }
   ->service { 'alertmanager':
     enable => true,
